428198e47d8f84fd1a3d75e1e055e8612e995b9e
bkirilov <bkirilov@shutterfly.com>
2018-10-30 11:10:46 +0200

    Init

---
+++ src/parts/PartsInfo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div> {{ part.title }} </div>
+        <div> {{ part.description }} </div>
+        <span> {{ part.cost }}</span>
+        <img :src="part.src" alt="">
+    </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+export default {
+  name: 'PartsInfo',
+  props: {
+    partType: { type: String },
+    id: {
+      type: [Number, String],
+      validator: value => Number.isInteger(Number(value)),
+    },
+  },
+  computed: {
+    part() {
+      const { partType, id } = this;
+
+      return parts[partType].find(part => part.id === +id);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="header">
+            <span v-if="open" @click="open = !open">Collapse</span>
+            <span v-if="!open" @click="open = !open">Expand</span>
+        </div>
+        <!-- Toggable content -->
+        <slot v-if="open">
+            <span>Default Content Goes Here and is not rendered if something is passed</span>
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return { open: true };
+  },
+};
+</script>
+
+<style>
+    .header {
+        background-color: #bbb;
+        padding: 3px;
+        cursor: pointer;
+    }
+</style>

3a262ffbcc659085bf42c6250410184519b9921e
Branimir123 <brani_svetoslavov@abv.bg>
2018-10-30 13:28:15 +0200

    Add sidebar

---
+++ src/sidebars/SidebarBuild.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="sidebar">
+        Build Sidebar
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Build',
+});
+</script>
+
+<style lang="scss">
+    .sidebar {
+        font-size: 50px;
+        transform: rotate(90deg);
+        color: green;
+    }
+</style>
+
---
+++ src/sidebars/SidebarStandard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="sidebar">
+        Standart Sidebar
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Standard',
+});
+</script>
+
+<style lang="scss">
+    .sidebar {
+        font-size: 50px;
+        transform: rotate(90deg);
+    }
+</style>
+
